95d4f9b09d3cbfe975213bb9bcf2dd6cb1866c13
Ben Limmer <ben@benlimmer.com>
2025-09-25 08:59:34 -0600

    single test

--- tests/enums.test.ts
+++ tests/enums.test.ts
@@ -1,7 +1,7 @@
 import { createTestSchema, runPlugin } from "./utils/test-helpers";
 
 describe("Enum types", () => {
-  it("generates enum types", async () => {
+  it("generates enum types with default and mapped values", async () => {
     const schema = createTestSchema(`
       enum Popularity {
         Low
@@ -13,38 +13,61 @@ describe("Enum types", () => {
         NO
       }
 
+      enum Status {
+        ACTIVE
+        INACTIVE
+      }
+
       type Author {
         name: String!
         popularity: Popularity!
         working: Working
       }
 
+      type User {
+        name: String!
+        status: Status!
+      }
+
       type Query {
         authors: [Author!]!
+        users: [User!]!
       }
     `);
 
-    const code = await runPlugin(schema);
+    const code = await runPlugin(schema, {
+      enumValues: {
+        Status: "./enums#StatusValues",
+      },
+    });
 
     expect(code).toMatchInlineSnapshot(`
      "import { GraphQLResolveInfo } from "graphql";
      import { Context } from "./context";
+     import { StatusValues } from "./enums";
 
      export interface Resolvers {
        Query: QueryResolvers;
        Author?: AuthorResolvers;
+       User?: UserResolvers;
      }
 
      export type UnionResolvers = {};
 
      export interface QueryResolvers {
        authors: Resolver<{}, {}, readonly Author[]>;
+       users: Resolver<{}, {}, readonly User[]>;
      }
 
      export interface AuthorResolvers {
        name: Resolver<Author, {}, string>;
        popularity: Resolver<Author, {}, Popularity>;
        working: Resolver<Author, {}, Working | null | undefined>;
+     }
+
+     export interface UserResolvers {
+       name: Resolver<User, {}, string>;
+       status: Resolver<User, {}, StatusValues>;
      }
 
      type MaybePromise<T> = T | Promise<T>;
@@ -66,6 +89,11 @@ describe("Enum types", () => {
        working: Working | null | undefined;
      }
 
+     export interface User {
+       name: string;
+       status: StatusValues;
+     }
+
      export enum Popularity {
        Low = "Low",
        High = "High",
@@ -74,71 +102,6 @@ describe("Enum types", () => {
      export enum Working {
        Yes = "YES",
        No = "NO",
-     }
-     "
-    `);
-  });
-
-  it("generates enums with custom values", async () => {
-    const schema = createTestSchema(`
-      enum Status {
-        ACTIVE
-        INACTIVE
-      }
-
-      type User {
-        name: String!
-        status: Status!
-      }
-
-      type Query {
-        users: [User!]!
-      }
-    `);
-
-    const code = await runPlugin(schema, {
-      enumValues: {
-        Status: "./enums#StatusValues",
-      },
-    });
-
-    expect(code).toMatchInlineSnapshot(`
-     "import { GraphQLResolveInfo } from "graphql";
-     import { Context } from "./context";
-     import { StatusValues } from "./enums";
-
-     export interface Resolvers {
-       Query: QueryResolvers;
-       User?: UserResolvers;
-     }
-
-     export type UnionResolvers = {};
-
-     export interface QueryResolvers {
-       users: Resolver<{}, {}, readonly User[]>;
-     }
-
-     export interface UserResolvers {
-       name: Resolver<User, {}, string>;
-       status: Resolver<User, {}, StatusValues>;
-     }
-
-     type MaybePromise<T> = T | Promise<T>;
-     export type Resolver<R, A, T> = (root: R, args: A, ctx: Context, info: GraphQLResolveInfo) => MaybePromise<T>;
-
-     export type SubscriptionResolverFilter<R, A, T> = (
-       root: R | undefined,
-       args: A,
-       ctx: Context,
-       info: GraphQLResolveInfo,
-     ) => boolean | Promise<boolean>;
-     export type SubscriptionResolver<R, A, T> = {
-       subscribe: (root: R | undefined, args: A, ctx: Context, info: GraphQLResolveInfo) => AsyncIterator<T>;
-     };
-
-     export interface User {
-       name: string;
-       status: StatusValues;
      }
 
      export { StatusValues } from "./enums";
